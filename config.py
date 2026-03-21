19413662b13ff53c43b2391c7a6c3d0ade85f86f
pranjalv23 <pranjal200186@gmail.com>
2026-03-21 18:04:09 +0530

    updated the code

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 AGENT_URLS = {
     "financial-agent": os.getenv("FINANCIAL_AGENT_URL", "http://localhost:9001"),
     "research-agent": os.getenv("RESEARCH_AGENT_URL", "http://localhost:9002"),
+    "interview-prep-agent": os.getenv("INTERVIEW_PREP_AGENT_URL", "http://localhost:9003"),
 }
 
 ROUTER_PROVIDER = os.getenv("ROUTER_PROVIDER", "groq")
