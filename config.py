6d7dc132e073b167c54b99c09580def1a4cbedeb
pranjalv23 <pranjal200186@gmail.com>
2026-03-31 18:40:18 +0530

    updated the repo

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ AGENT_URLS = {
     "research-agent": os.getenv("RESEARCH_AGENT_URL", "http://localhost:9002"),
     "interview-prep-agent": os.getenv("INTERVIEW_PREP_AGENT_URL", "http://localhost:9003"),
 }
-
-ROUTER_PROVIDER = os.getenv("ROUTER_PROVIDER", "groq")
